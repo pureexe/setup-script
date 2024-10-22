fcbfe6d88dc6b80cfd0ea32db42a19f88e551a5f
Pakkapon Phongthawee <pure.gif@gmail.com>
2024-10-22 20:24:42 +0700

    update command line

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
 
+# install pdsh
+
+# install zsh 
+
+# install oh-my-zsh
+
+# install fzf
+
+# install pure theme
+
+# install neovim
+
 # install zsh auto suggestion
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+# setup custom command ducks and ul
