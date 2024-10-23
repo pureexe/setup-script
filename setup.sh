e57a41ba55b832707d01707dd1f529ea47d7e164
Pakkapon Phongthawee <pure.gif@gmail.com>
2024-10-23 20:28:14 +0700

    save

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 
+# install brew
+
 # install pdsh
 
 # install zsh 
@@ -16,3 +18,13 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 
 # setup custom command ducks and ul
+
+
+# plan to install 
+# atuin # for key up
+
+
+# dust for file what taking space
+
+# setup powerline theme for tmux 
+#https://github.com/jimeh/tmux-themepack/tree/master
